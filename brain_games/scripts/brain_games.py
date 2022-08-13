7ebf0597ccd7e7ef95d745b3ce773a9dd6d297b2
iliya sinitcyn <baner1337228@gmail.com>
2022-08-13 16:33:05 +0300

    add poetry to my project, and made a greeting

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+
+from brain_games.cli import welcome_user
+
+
 def bg():
     print('Welcome to the Brain Games!')
 
 def main():
     bg()
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
