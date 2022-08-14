d7856aa70fa0440363d3ea221e8e07dda48eab44
iliya sinitcyn <baner1337228@gmail.com>
2022-08-14 14:49:16 +0300

    fixed version compatibility issue

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,10 +6,10 @@ from brain_games.cli import welcome_user
 def bg():
     print('Welcome to the Brain Games!')
 
+
 def main():
     bg()
     welcome_user()
-
 
 if __name__ == '__main__':
     main()
