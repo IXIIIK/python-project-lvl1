38e752e5d4b9f7704046d4fb255709aee2d1d6dc
iliya sinitcyn <baner1337228@gmail.com>
2022-08-21 18:07:27 +0300

    make a new game Brain-gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -2,23 +2,58 @@ import prompt
 import random
 from brain_games.scripts.logic import random_num
 
+
+print('Welcome to the Brain Games!')
+name = prompt.string('May i have your name?: ')
+print(f'Hello, {name}!')
+print('Find the greatest common divisor of given numbers.')
+
 def main():
     pass
 
+def gcd(c):
+
+    (a, b) = c
+
+    while a != b:
+
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
+
+
+
+
 def brain_gcd():
 
-    num1 = random_num(20, 100)
-    num2 = random_num(20, 100)
+    i = 0
+    winstrike = 3
 
-    print(f'Question: {num1} {num2}')
+    while i < winstrike:
+
+        i += 1
+
+        num1 = random_num(20, 100)
+        num2 = random_num(20, 100)
+
+        correct_answer = gcd((num1, num2))
+
+        try:
+            print(f'Question: {num1} {num2}')
+            usr_answer = prompt.string('Your answer: ')
 
 
-    while num1 != num2:
+            if int(usr_answer) == gcd((num1, num2)):
+                print('Correct!')
+            else:
+                return print(f"'{usr_answer}'is wrong answer ;(. Correct answer was '{correct_answer}'.\nLets try again")
 
-        if num1 > num2:
-            num1 -= num2
-        else:
-            num2 -= num1
-        print(num2)
+        except ValueError:
+            return print(f"'{usr_answer}' is wrong answer ;(, {name}!")
+
+    return 'Congratulations'
+
 
 brain_gcd()
