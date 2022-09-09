70d4d114b4b7a48ca9d81a04b9f37391eac99d2a
iliya sinitcyn <baner1337228@gmail.com>
2022-09-09 17:20:00 +0300

    fox

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,51 +1,6 @@
 #!/usr/bin/env python3
 
-import prompt
-import random
-from sympy import isprime
-from brain_games.logic.games_logic import random_num
-
-
-print('Welcome to the Brain Games!')
-name = prompt.string('May I have your name? ')
-print(f'Hello, {name}!')
-print('Answer "yes" if given number is prime. Otherwise answer "no".')
-
-
-def prime(number):
-
-    if isprime(number) is True:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-    return correct_answer
-
-
-
-def prime_game():
-    i = 0
-    winstrike = 3
-
-    while i < winstrike:
-
-        number = random.randrange(0, 100)
-
-        correct_answer = prime(number)
-
-        print(f"Question: {number}")
-        usr_answer = prompt.string('Your answer: ')
-
-        if correct_answer == 'yes' and usr_answer == 'yes' or (
-            correct_answer == 'no' and usr_answer == 'no'):
-            print('Correct!')
-            i += 1
-        else:
-            print(f"'{usr_answer}' is wrong answer ;(.\
-Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
-            break
-
-    if i == 3:
-        print(f'Congratulations, {name}!')
+from brain_games.games.prime_game import prime_game
 
 
 def main():
