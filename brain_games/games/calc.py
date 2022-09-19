297322d26a760ec006391f3d26190564ed54fcfc
iliya sinitcyn <baner1337228@gmail.com>
2022-09-19 15:25:48 +0300

    fix

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,11 +2,27 @@ import prompt
 import random
 
 opper = ('+', '-', '*')
+opperator = random.choice(opper)
+
 
 print('Welcome to the Brain Games!')
 name = prompt.string('May I have your name? ')
 print(f'Hello, {name}!')
 print('What is the result of the expression?')
+
+
+def calc_question(num1, num2):
+
+    if opperator == '+':
+        correct_answer = num1 + num2
+
+    elif opperator == '-':
+        correct_answer = num1 - num2
+
+    elif opperator == '*':
+        correct_answer = num1 * num2
+
+    return correct_answer
 
 
 def calc_game():
@@ -18,38 +34,19 @@ def calc_game():
 
         num1 = random.randint(0, 25)
         num2 = random.randint(0, 10)
-        correct_answer = (num1 + num2, num1 - num2, num1 * num2)
-        opperator = random.choice(opper)
+        question = f'{num1} {opperator} {num2}'
+        correct_answer = calc_question(num1, num2)
 
-        try:
-            calc = f'{num1} {opperator} {num2}'
-            print(f'Question: {calc}')
-            usr_answer = prompt.string('Your answer: ')
-            usr_answer = int(usr_answer)
+        print(f'Question: {question}')
+        usr_answer = prompt.string('Your answer: ')
 
-            if num1 + num2 == usr_answer or (
-                    num1 - num2 == usr_answer) or (
-                        num1 * num2 == usr_answer):
-                print('Correct!')
-                i += 1
+        if int(usr_answer) == correct_answer:
+            print('Correct!')
+            i += 1
 
-            elif usr_answer != num1 + num2 and opperator == "+":
-                print(f"'{usr_answer}' is wrong answer ;(.\
-Correct answer was '{correct_answer[0]}'.\nLet's try again, {name}!")
-                break
-
-            elif usr_answer != num1 - num2 and opperator == "-":
-                print(f"'{usr_answer}' is wrong answer ;(.\
-Correct answer was '{correct_answer[1]}'.\nLet's try again, {name}!")
-                break
-
-            elif usr_answer != num1 * num2 and opperator == "*":
-                print(f"'{usr_answer}' is wrong answer ;(.\
-Correct answer was '{correct_answer[2]}'.\nLet's try again, {name}!")
-                break
-
-        except ValueError:
-            print(f"'{usr_answer}' is wrong answer ;(, {name}!")
+        elif usr_answer == str(usr_answer):
+            print(f"'{usr_answer}' is wrong answer ;(.\
+Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
             break
 
     if i == 3:
