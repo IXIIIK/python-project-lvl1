8b3da65aacff83ffa3c37150b7875c5c13f5d616
iliya sinitcyn <baner1337228@gmail.com>
2022-10-23 11:36:34 +0300

    rename a game

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.games import calc
+from brain_games.games import calc_game
 from brain_games.games_logic import run_game
 
 
 def main():
-    run_game(calc)
+    run_game(calc_game)
 
 
 if __name__ == '__main__':
